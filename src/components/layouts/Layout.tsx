95d9b2cbb93eb117c08a2eff676a516338a95fa9
Salomon Moreno <msalomon@vmware.com>
2023-04-11 08:08:07 -0500

    Added Open graphs meta tags

--- src/components/layouts/Layout.tsx
+++ src/components/layouts/Layout.tsx
@@ -1,10 +1,13 @@
 import Head from "next/head";
-import { FC, PropsWithChildren } from "react";
+import { FC, PropsWithChildren, useEffect } from "react";
 import { Navbar } from "../ui";
+import { useRouter } from "next/router";
 
 interface Props {
   title?: string;
 }
+
+const origin = typeof window === "undefined" ? "" : window.location.origin;
 
 export const Layout: FC<PropsWithChildren<Props>> = ({ children, title }) => {
   return (
@@ -16,6 +19,12 @@ export const Layout: FC<PropsWithChildren<Props>> = ({ children, title }) => {
           name="description"
           content={`Informacion sobre el pokemon ${title || "Unknown"}`}
         />
+        <meta property="og:title" content="Find your pokemon" />
+        <meta
+          property="og:description"
+          content="The best way to lookup for your chilhood pokemon"
+        />
+        <meta property="og:image" content={`${origin}/images/banner.png`} />
         <meta
           name="keywords"
           content={`${title || "Unknown"}, pokemon, pokedex`}
